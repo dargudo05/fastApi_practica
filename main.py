d6ef24467572f241ecc26a8b94a1c684536e3442
dargudo <daniel.argudo@sasf.net>
2026-07-08 17:19:54 -0500

    add Task model and implement create/get task endpoints

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import os
+
 from fastapi import FastAPI, Form, Response, HTTPException, status
 from typing import Annotated
 from pydantic import BaseModel
 from models.item import Item
+from models.task import Task
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
+supabase: Client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_PUBLISHABLE_KEY"))
 
 
 class FormData(BaseModel):
@@ -128,3 +136,16 @@ def create_item(
         content=message,
         status_code=201
     )
+
+@app.post("/tasks/")
+def create_task(task: Task):
+  data = supabase.table("task").insert({
+      "title": task.title,
+      "description": task.description
+  }).execute()
+  return data.data
+
+@app.get("/tasks/")
+def get_tasks():
+   data = supabase.table("task").select("*").execute()
+   return data.data
